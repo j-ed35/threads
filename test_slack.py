5651bb5492bf12521806ac58d5436957cfb8aa2a
Jacob Ederer <jederer35@gmail.com>
2025-10-22 11:03:47 -0400

    Update test_slack with today's NBA games

--- test_slack.py
+++ test_slack.py
@@ -13,14 +13,54 @@ def test_slack():
         slack = SlackClient()
 
         # Mock game data
-        mock_games = """Houston Rockets :HOU: at Oklahoma City Thunder :OKC: | 7:30 PM ET
-        - :reminder_ribbon: REMINDER
-        - :GTD: GTD/QUESTIONABLE
-        - :out: INJURIES
-Golden State Warriors :GSW: at Los Angeles Lakers :LAL: | 10:00 PM ET
-        - :reminder_ribbon: REMINDER
-        - :GTD: GTD/QUESTIONABLE
-        - :out: INJURIES"""
+        mock_games = """Cleveland Cavaliers (0-0) :CLE-: at New York Knicks (0-0) :NYK: | 7:00 PM ET
+                - :reminder_ribbon: REMINDER
+                - :GTD: GTD/QUESTIONABLE
+                - :out: INJURIES
+        San Antonio Spurs (0-0) :SAS: at Dallas Mavericks (0-0) :DAL: | 9:30 PM ET
+                - :reminder_ribbon: REMINDER
+                - :GTD: GTD/QUESTIONABLE
+                - :out: INJURIES
+        Brooklyn Nets (0-0) :BKN: at Charlotte Hornets (0-0) :CHA: | 7:00 PM ET
+                - :reminder_ribbon: REMINDER
+                - :GTD: GTD/QUESTIONABLE
+                - :out: INJURIES
+        Miami Heat (0-0) :MIA: at Orlando Magic (0-0) :ORL: | 7:00 PM ET
+                - :reminder_ribbon: REMINDER
+                - :GTD: GTD/QUESTIONABLE
+                - :out: INJURIES
+        Toronto Raptors (0-0) :TOR-: at Atlanta Hawks (0-0) :ATL: | 7:30 PM ET
+                - :reminder_ribbon: REMINDER
+                - :GTD: GTD/QUESTIONABLE
+                - :out: INJURIES
+        Philadelphia 76ers (0-0) :PHI: at Boston Celtics (0-0) :BOS: | 7:30 PM ET
+                - :reminder_ribbon: REMINDER
+                - :GTD: GTD/QUESTIONABLE
+                - :out: INJURIES
+        Detroit Pistons (0-0) :DET: at Chicago Bulls (0-0) :CHI: | 8:00 PM ET
+                - :reminder_ribbon: REMINDER
+                - :GTD: GTD/QUESTIONABLE
+                - :out: INJURIES
+        New Orleans Pelicans (0-0) :NOP: at Memphis Grizzlies (0-0) :MEM: | 8:00 PM ET
+                - :reminder_ribbon: REMINDER
+                - :GTD: GTD/QUESTIONABLE
+                - :out: INJURIES
+        Washington Wizards (0-0) :WAS: at Milwaukee Bucks (0-0) :MIL: | 8:00 PM ET
+                - :reminder_ribbon: REMINDER
+                - :GTD: GTD/QUESTIONABLE
+                - :out: INJURIES
+        LA Clippers (0-0) :LAC: at Utah Jazz (0-0) :UTA: | 9:00 PM ET
+                - :reminder_ribbon: REMINDER
+                - :GTD: GTD/QUESTIONABLE
+                - :out: INJURIES
+        Sacramento Kings (0-0) :SAC: at Phoenix Suns (0-0) :PHX: | 10:00 PM ET
+                - :reminder_ribbon: REMINDER
+                - :GTD: GTD/QUESTIONABLE
+                - :out: INJURIES
+        Minnesota Timberwolves (0-0) :MIN: at Portland Trail Blazers (0-0) :POR: | 10:00 PM ET
+                - :reminder_ribbon: REMINDER
+                - :GTD: GTD/QUESTIONABLE
+                - :out: INJURIES"""
 
         print("Sending test message to Slack...")
         result = slack.send_games(mock_games)
